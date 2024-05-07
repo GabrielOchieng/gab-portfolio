513e85efcc3f769cceba1b7f7a3998af25b14170
GabrielOchieng <ogingagabriel@gmail.com>
2024-05-07 12:26:59 +0300

    refactor

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -50,7 +50,7 @@ const Body = () => {
               </div>
             </Link>
             <a
-              href="https://drive.google.com/file/d/1cfwVqOo4NyjJPKyUqm4USox2Ch_QsALC/view"
+              href="https://drive.google.com/file/d/17ge3yLNpmLbnXOHxMaVzktJ1zDzg6MXu/view?usp=drive_link"
               target="_blank"
             >
               <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-105 ease-in duration-300">
