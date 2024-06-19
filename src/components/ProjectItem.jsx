519b09da0bca7b8cdb0495cfcf3ca219a666d40c
GabrielOchieng <ogingagabriel@gmail.com>
2024-06-19 11:47:32 +0300

    Addition of latest project

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -1,6 +1,53 @@
+// import Image from "next/image";
+// import Link from "next/link";
+// import React from "react";
+
+// const ProjectItem = ({
+//   title,
+//   backgroundImg,
+//   projectUrl,
+//   projectPara,
+//   projectCode,
+// }) => {
+//   return (
+//     <div
+//       className="relative flex items-center justify-center h-auto w-full shadow-xl
+//         shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-[#5651e5] to-[#709dff]"
+//     >
+//       <Image
+//         className="rounded-xl group-hover:opacity-10"
+//         src={backgroundImg}
+//         alt="/"
+//       />
+//       <div className="hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
+//         <h3 className="text-2xl text-white tracking-wider text-center">
+//           {title}
+//         </h3>
+//         <p className="pb-4 pt-2 text-white text-center">{projectPara}</p>
+//         <div className="flex gap-3">
+//           <Link href={projectCode} target="_blank" rel="noopener noreferrer">
+//             <p className="text-center py-3 rounded-lg  text-gray-700 font-bold text-lg cursor-pointer">
+//               <button className="px-8 py-2 mt-4">Code</button>
+//             </p>
+//           </Link>
+//           <Link href={projectUrl} target="_blank" rel="noopener noreferrer">
+//             <p className="text-center py-3 rounded-lg text-gray-700 font-bold text-lg cursor-pointer">
+//               <button className="px-8 py-2 mt-4">Demo</button>
+//             </p>
+//           </Link>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default ProjectItem;
+
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react"; // Import useState hook
 
 const ProjectItem = ({
   title,
@@ -8,35 +55,70 @@ const ProjectItem = ({
   projectUrl,
   projectPara,
   projectCode,
+  description, // Add a new prop for project description
 }) => {
+  const [showDetails, setShowDetails] = useState(false); // State for modal visibility
+
+  const handleOpenDetails = () => setShowDetails(true); // Function to open modal
+  const handleCloseDetails = () => setShowDetails(false); // Function to close modal
+
   return (
-    <div
-      className="relative flex items-center justify-center h-auto w-full shadow-xl 
-        shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-[#5651e5] to-[#709dff]"
-    >
+    <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-[#5651e5] to-[#709dff]">
       <Image
         className="rounded-xl group-hover:opacity-10"
         src={backgroundImg}
         alt="/"
       />
-      <div className="hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
-        <h3 className="text-2xl text-white tracking-wider text-center">
+      <div className="hidden group-hover:flex flex-col items-center absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
+        <h3 className=" text-xl md:text-2xl text-white tracking-wider text-center">
           {title}
         </h3>
-        <p className="pb-4 pt-2 text-white text-center">{projectPara}</p>
-        <div className="flex gap-3">
+        <p className="pb-2 md:pb-4 pt-2 text-white text-center">
+          {projectPara}
+        </p>
+        <div className="flex gap-1 md:gap-3">
           <Link href={projectCode} target="_blank" rel="noopener noreferrer">
-            <p className="text-center py-3 rounded-lg  text-gray-700 font-bold text-lg cursor-pointer">
-              <button className="px-8 py-2 mt-4">Code</button>
+            <p className="text-center py-1 md:py-3 rounded-lg text-gray-700 font-bold text-lg cursor-pointer">
+              <button className="px-8 py-1 mt-1 md:mt-4 hover:underline">
+                Code
+              </button>
             </p>
           </Link>
           <Link href={projectUrl} target="_blank" rel="noopener noreferrer">
-            <p className="text-center py-3 rounded-lg text-gray-700 font-bold text-lg cursor-pointer">
-              <button className="px-8 py-2 mt-4">Demo</button>
+            <p className="text-center py-1 md:py-3 rounded-lg text-gray-700 font-bold text-lg cursor-pointer">
+              <button className="px-8 py-1 mt-1 md:mt-4 hover:underline">
+                Demo
+              </button>
             </p>
           </Link>
         </div>
+        <div>
+          {" "}
+          <button
+            className=" text-center rounded-lg text-white font-bold text-lg cursor-pointer px-4 md:px-8 py-1 md:py-2 mt-2 md:mt-4 hover:underline"
+            onClick={handleOpenDetails}
+          >
+            View More
+          </button>
+        </div>
       </div>
+
+      {/* Modal content (wrapped in a conditional statement) */}
+      {showDetails && (
+        <div className="absolute inset-0 bg-gray-500 text-white bg-opacity-75 flex items-center justify-center z-50">
+          <div className="bg-gradient-to-r from-[#5651e5] to-[#709dff] rounded-lg shadow-lg p-4 w-full max-w-md">
+            <h2 className="text-xl font-bold mb-4">{title}</h2>
+            <p className="mb-4">{description}</p>
+            {/* Display project description */}
+            <button
+              className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-700"
+              onClick={handleCloseDetails}
+            >
+              Close
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -23,20 +23,26 @@ const Projects = () => {
             projectUrl="https://willycollections.vercel.app"
             projectCode="https://github.com/GabrielOchieng/willycollections"
             projectPara="React, Firebase, Bootstrap & React Context"
+            description="This is a full-featured React e-commerce application that allows users to browse, search, add items to carts, checkout, and manage orders.Bootstrap is used for the styling providing a clean and user-friendly interface. Customers can easily browse through product categories, view detailed descriptions and images, all with a few clicks! They can also select the products they want and add them to their shopping cart. Firebase is also used to manage the backend stuff. It manages user accounts, shopping carts, and even order making and confirmation. This allows for a secure and streamlined checkout process, making it easy for customers to complete their orders"
           />
+
           <ProjectItem
             title="TASK MANAGEMENT WEBSITE"
             backgroundImg={task}
             projectUrl="https://task-manager-r6wk.onrender.com"
             projectCode="https://github.com/GabrielOchieng/task-manager"
             projectPara="React, Tailwind, Redux, Node.js, Express, MongoDB"
+            description="This website helps manage tasks and teams with different user roles. Users can register and then the admin assigns 'manager' roles to some of them. Managers can then control tasks and departments for their teams. They can create new departments, edit existing ones, or even delete them if needed. They can also add, change, or remove tasks assigned to their team members. The users can also view tasks assigned to them and mark them as complete after which they are removed from the job-board. To help managers stay on top of things, the website provides a special dashboard. This dashboard uses charts and bars to show how well the teams and departments are doing. This way, managers can easily see where things stand and make adjustments as needed"
           />
+
           <ProjectItem
             title="PHOTOWEB"
             backgroundImg={photoweb}
             projectUrl="https://photowebapp.onrender.com"
             projectCode="https://github.com/GabrielOchieng/photoweb.git"
             projectPara="Reactjs, Tailwind, Redux, Node.js, Express & MongoDB"
+            description="Photoweb is a user-friendly web app, built with React and Node, Express and MongoDB to handle the authentication, Tailwind CSS ensures a beautiful and responsive user interface, making browsing your memories a joy, and Redux for state management. Securely register and login to easily fetch photos from JSON placeholder and view them!
+"
           />
           <ProjectItem
             title="CLEARLINK"
@@ -44,6 +50,7 @@ const Projects = () => {
             projectUrl="https://clear-link-gabrielochieng.vercel.app"
             projectCode="https://github.com/GabrielOchieng/ClearLink"
             projectPara="HTML, CSS & JavaScript"
+            description="A landing page website for clearlink converted from a figma file"
           />
           <ProjectItem
             title="ELCK CDC WEBSITE"
@@ -51,6 +58,7 @@ const Projects = () => {
             projectUrl="https://elck-cdc.onrender.com/"
             projectCode="https://github.com/GabrielOchieng/elck-frontend"
             projectPara="React, CSS, NodeJs, Express & MongoDB"
+            description="This project is the frontend of the ELCK CDC website, that is built with the MERN stack. It has a landing page displaying what the organization does, about it, how to contribute, its partners and the testimonials. You can also move to the welcome page where you will be required to register as a user after which you will be able to sign in and view the participants with their details and the activities photos. Furthermore, you can Create, Update and Delete participants/photos as an admin and not as a normal user as these roles are restricted to those registered as admins of the database."
           />
           <ProjectItem
             title="SKILLSCONNECT"
