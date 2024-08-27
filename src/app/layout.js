64045b45a17bd8a5a0e9bad3b6c9ec6bcf2ed48d
GabrielOchieng <ogingagabriel@gmail.com>
2024-08-27 13:37:40 +0300

    modification of header

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,7 +5,7 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Gabriel | Front-End Developer",
+  title: "Gabriel | Software Developer",
 };
 
 export default function RootLayout({ children }) {
